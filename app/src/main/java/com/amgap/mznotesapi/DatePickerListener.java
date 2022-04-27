73141da1e4eb12b4609b4b748388fe3f20e4ee5c
DWMarkus <gapasin.marc@yahoo.fr>
2022-04-27 16:11:44 +0200

    DatePickerListener

--- app/src/main/java/com/amgap/mznotesapi/DatePickerListener.java
+++ app/src/main/java/com/amgap/mznotesapi/DatePickerListener.java
@@ -1,4 +1,25 @@
 package com.amgap.mznotesapi;
 
-public class DatePickerListener {
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+public class DatePickerListener implements DatePickerDialog.OnDateSetListener {
+    private final TextView dateText;
+
+    public DatePickerListener(TextView dateText) {
+        this.dateText = dateText;
+    }
+
+    private String addLeadingZero(int i) {
+        return i < 10 ? "0" + i : String.valueOf(i);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        final String formattedDay = addLeadingZero(dayOfMonth);
+        final String formattedMonth = addLeadingZero(month + 1);
+        final String date = formattedDay + "/" + formattedMonth + "/" + year;
+        dateText.setText(date);
+    }
 }
