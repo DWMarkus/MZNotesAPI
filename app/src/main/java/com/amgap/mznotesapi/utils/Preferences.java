acfc2fb1ae762d20294ed486cd5e1c92870059db
DWMarkus <gapasin.marc@yahoo.fr>
2022-04-27 16:15:18 +0200

    Preferences + gradle

---
+++ app/src/main/java/com/amgap/mznotesapi/utils/Preferences.java
@@ -0,0 +1,13 @@
+package com.amgap.mznotesapi.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.preference.PreferenceManager;
+import com.github.antoinejt.exassert.ExAssert;
+
+public class Preferences {
+    private Preferences() {
+        // camoufle par défaut
+    }
+}
